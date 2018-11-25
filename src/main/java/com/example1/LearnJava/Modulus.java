b2281bbd5e2906539dc77c8490ffeba68f19c922
unknown <unlikushi@gmail.com>
2018-11-25 18:23:05 +0530

    learn commit

--- src/main/java/com/example1/LearnJava/Modulus.java
+++ src/main/java/com/example1/LearnJava/Modulus.java
@@ -3,7 +3,7 @@ package com.example1.LearnJava;
 public class Modulus {
     public void Modulus() {
         int x = 20;
-        int y = 2;
+        int y = 3;
         int result = x % y;
         System.out.println("modulus =" +result);
     }
